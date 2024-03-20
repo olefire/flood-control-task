9a77bad061c19b636279a514d8b252aca2aba2dd
olefire <109245281+olefire@users.noreply.github.com>
2024-03-20 23:24:47 +0300

    feat: made structures

---
+++ internal/ratelimiter/interfaces.go
@@ -0,0 +1,7 @@
+package ratelimiter
+
+import "context"
+
+type FloodControl interface {
+	Check(ctx context.Context, userID int64) (bool, error)
+}
--- internal/ratelimiter/ratelimiter.go
+++ internal/ratelimiter/ratelimiter.go
@@ -1,1 +1,20 @@
 package ratelimiter
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+type Deps struct {
+	*redis.Client
+	FloodControl
+}
+
+type LimiterClient struct {
+	Deps
+}
+
+func New(d Deps) *LimiterClient {
+	return &LimiterClient{
+		Deps: d,
+	}
+}
